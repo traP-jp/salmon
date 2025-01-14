647adc417721aca0db2e2665781dca1b3f2be953
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2025-01-14 09:55:35 +0900

    役員会25 に update

--- server/bot/const.go
+++ server/bot/const.go
@@ -3,5 +3,5 @@ package bot
 const (
 	AgreeStampId     = "1545b5f4-0766-4b43-b9ef-0ce8b66ad8c2"
 	DisagreeStampId  = "f89f2861-5882-4d99-a4c9-d119f9e29468"
-	ExecutiveGroupId = "66d1e94d-0d27-44e7-bb85-771507454cad"
+	ExecutiveGroupId = "01937db6-977f-7c92-823e-a22f6e6a308b" // executive25
 )
